fa3f2b1dbbdfdc21e997497566e6e80eb3858716
Kubra Ucar <kvd.19998@gmail.com>
2024-08-26 21:53:27 +0300

    modalkonumlandırma

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 // Elemanları seçme
-const openModalButton = document.getElementById('openModalButton');
+const openModalButtons = document.querySelectorAll('.openModalButton');
 const modal = document.getElementById('myModal');
 const closeButton = document.querySelector('.close-button');
 const modalContent = document.getElementById('modalContent'); // Modal içeriği bölümü
 const modalcontent=document.querySelector('.modal-content');
 
+
+
 // Modali açma ve konumlandırma
-openModalButton.addEventListener('click', function() {
+
+openModalButtons.forEach(button => {
+button.addEventListener('click', function(event){
 
     modalcontent.style.display='block';
 
 
 
-    const rect = openModalButton.getBoundingClientRect(); // Butonun konumunu al
+    const rect = button.getBoundingClientRect(); // Butonun konumunu al
     modal.style.display = 'block';
+    modal.style.top = `${rect.top + window.scrollX}px`; // Butonun hemen altına yerleştir
+    modal.style.left = `${rect.right + window.scrollY+1000}px`; // Butonun sol hizasında yerleştir
 
    
 });
+});
+
 
 
 // Modali kapatma
